8003579efb4ce5caa2d17ca31d64049fe557bf81
haphut <haphut@mistmap.com>
2020-10-01 12:15:46 +0300

    Add init-exercises.sh

---
+++ scripts/init-exercises.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -Eeuxo pipefail
+
+notebook_urls=(
+  'https://raw.githubusercontent.com/mistmap/postgresql-training-finnish-2020-10/main/notebooks/jsonb.ipynb'
+  'https://raw.githubusercontent.com/mistmap/postgresql-training-finnish-2020-10/main/notebooks/ohjelmointitekniikoita.ipynb'
+)
+
+if [ "${DO_INIT_EXERCISES}" != 'true' ] ; then
+  echo >&2 "DO_INIT_EXERCISES is not set to 'true'. Skip initialization."
+  exit 0
+fi
+
+for url in "${notebook_urls[@]}"; do
+  wget \
+    --timestamping \
+    --directory-prefix='/exercises' \
+    "${url}"
+done
